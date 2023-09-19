f26747f1dad6238d73444b04ec70f81177c4078b
devjyu <devjyu@gmail.com>
2023-09-19 10:13:50 +0900

    조건부 렌더링-삼항연산자

--- test/src/App.js
+++ test/src/App.js
@@ -5,10 +5,10 @@ import Wrapper from './Wrapper';
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red"/>
-      <Hello color="pink"/>
+      <Hello name="react" color="red" isSpecial={true}/>
+      <Hello color="pink" />
     </Wrapper>
-  );
+  )
 }
 
 export default App;
