b0c2ae4fd8e578ff95fe077a4ee96cc450427e7f
devjyu <devjyu@gmail.com>
2023-09-19 09:46:43 +0900

    CSS class 를 설정

--- test/src/App.js
+++ test/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hello from './hello';
+import './App.css';
 
 function App() {
   const name = 'react';
@@ -14,6 +15,7 @@ function App() {
     <>
       <Hello />
       <div style={style}>{name}</div>
+      <div className="gray-box"></div>
     </>
   );
 }
